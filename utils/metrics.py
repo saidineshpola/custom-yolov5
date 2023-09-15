a5ad4cc632434fa30b378e3bfd2663a00742715b
saidinesh_pola <saidinesh_pola@noreply.gitlab.aicrowd.com>
2023-09-15 08:24:00 +0530

    modifed fitness of evolve

--- utils/metrics.py
+++ utils/metrics.py
@@ -16,7 +16,7 @@ from utils import TryExcept, threaded
 
 def fitness(x):
     # Model fitness as a weighted combination of metrics
-    w = [0.2,0.4,0.3,0.1] #[0.0, 0.0, 0.1, 0.9]  # weights for [P, R, mAP@0.5, mAP@0.5:0.95]
+    w = [0.4,0.3,0.3,0] #[0.0, 0.0, 0.1, 0.9]  # weights for [P, R, mAP@0.75, mAP@0.75:0.95]
     return (x[:, :4] * w).sum(1)
 
 
